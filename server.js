35b86b716db75351e58a3d6731722fe63f5a30f4
C.ifegwu <C.ifegwu@alustudent.com>
2025-11-22 18:09:27 +0200

    Update

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`ChopNow server is running on port ${PORT}`);
     console.log(`API Base URL: http://localhost:${PORT}/api`);
 });
