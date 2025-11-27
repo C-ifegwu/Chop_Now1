d1436cbde81fb530f646180311bf6d5f6d1213ab
C.ifegwu <C.ifegwu@alustudent.com>
2025-11-27 13:31:18 +0200

    Fixing bugs

--- chopnow-frontend/src/types/order-item.ts
+++
@@ -1,9 +0,0 @@
-export interface OrderItem {
-  id: number;
-  order_id: number;
-  meal_id: number;
-  quantity: number;
-  price: number;
-  meal_name: string;
-  image_url: string;
-}
--- chopnow-frontend/src/types/user.ts
+++
@@ -1,9 +0,0 @@
-export interface User {
-  id: number;
-  email: string;
-  user_type: 'consumer' | 'vendor';
-  phone: string;
-  name: string;
-  business_name: string;
-  address: string;
-}
